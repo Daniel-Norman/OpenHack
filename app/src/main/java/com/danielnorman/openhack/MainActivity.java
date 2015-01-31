5b2dd88277e371ea72868eed1b63ddbd3a48f365
Daniel Norman <danielnorman@ucla.edu>
2015-01-31 03:31:00 -0800

    Added CameraHandler, taking pictures

--- app/src/main/java/com/danielnorman/openhack/MainActivity.java
+++ app/src/main/java/com/danielnorman/openhack/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,16 +13,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.danielnorman.openhack.Handlers.CameraHandler;
 import com.danielnorman.openhack.Handlers.LocationHandler;
 import com.danielnorman.openhack.Handlers.ParseHandler;
 import com.parse.Parse;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends ActionBarActivity {
     private static int RESULT_LOAD_IMAGE = 1;
+    public static int REQUEST_TAKE_PHOTO = 2;
 
     LocationHandler mLocationHandler;
+    CameraHandler mCameraHandler;
     MapFragment mMapFragment;
     ListViewFragment mListViewFragment;
     PostFragment mPostFragment;
@@ -37,6 +45,8 @@ public class MainActivity extends ActionBarActivity {
         Parse.initialize(this, "QFGpI1loRkUxQSqPq6L3BRvMczGjsQGh1halYtej", "TuPPTR97s9hZbvcQi21Cfy5bpJJam4VKUhvfyMbm");
 
         mLocationHandler = new LocationHandler(this);
+        mCameraHandler = new CameraHandler(this);
+
         mMapFragment = new MapFragment();
         mListViewFragment = new ListViewFragment();
         mPostFragment = new PostFragment();
@@ -79,26 +89,90 @@ public class MainActivity extends ActionBarActivity {
 
     public void onClickPost(View view) {
         addFragment(mPostFragment);
+        pickImage();
     }
 
-    public void pickImage(View view) {
-        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    public void pickImage() {
+        dispatchTakePictureIntent();
+
+        //Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //startActivityForResult(i, RESULT_LOAD_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("ActivityResult");
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
-            final Uri selectedImage = data.getData();
+            final Uri imageUri = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 40, outputStream);
                 byte[] imageData = outputStream.toByteArray();
 
                 ParseHandler.postToParse(mLocationHandler, imageData);
             } catch (Exception e) { }
+        }
+        if (requestCode == REQUEST_TAKE_PHOTO)
+        {
+            //System.out.println(data.getData());
+        }
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Uri imageUri = Uri.fromFile(mImageFile);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 40, outputStream);
+                byte[] imageData = outputStream.toByteArray();
+
+                ParseHandler.postToParse(mLocationHandler, imageData);
+            } catch (Exception e) { };
+
+        }
+    }
+
+    String mCurrentPhotoPath;
+    File mImageFile;
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                mImageFile = photoFile;
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 }
